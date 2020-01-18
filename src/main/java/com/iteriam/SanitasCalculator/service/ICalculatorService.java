44866e5ecc58b044e7e405099533ecfe89548e64
esuvarian <esuvarian@ESuvarian.mshome.net>
2020-01-18 12:39:37 +0100

    Creado interfaz ICalculator

---
+++ src/main/java/com/iteriam/SanitasCalculator/service/ICalculatorService.java
@@ -0,0 +1,10 @@
+package com.iteriam.SanitasCalculator.service;
+
+import org.springframework.stereotype.Service;
+import com.iteriam.SanitasCalculator.model.BasicOperandModel;
+
+@Service
+public interface ICalculatorService {
+
+	public BasicOperandModel calculate(BasicOperandModel operand);
+}
