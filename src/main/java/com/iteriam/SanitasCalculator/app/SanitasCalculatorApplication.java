8b93fe1a5de17a9fdf3ebd5284674f5d042e78ea
esuvarian <esuvarian@ESuvarian.mshome.net>
2020-01-18 11:51:45 +0100

    Creación del proyecto y estructuración de capas

---
+++ src/main/java/com/iteriam/SanitasCalculator/app/SanitasCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.iteriam.SanitasCalculator.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanitasCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SanitasCalculatorApplication.class, args);
+	}
+
+}
