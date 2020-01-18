31c191e2586788188fe72541ee26b0c60d487c89
esuvarian <esuvarian@ESuvarian.mshome.net>
2020-01-18 13:10:55 +0100

    Creada la llamada a la capa de service desde la capa de controller

--- src/main/java/com/iteriam/SanitasCalculator/controller/OperatorController.java
+++ src/main/java/com/iteriam/SanitasCalculator/controller/OperatorController.java
@@ -1,23 +1,33 @@
 package com.iteriam.SanitasCalculator.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import com.iteriam.SanitasCalculator.model.BasicOperandModel;
+import com.iteriam.SanitasCalculator.service.AddCalculatorService;
+import com.iteriam.SanitasCalculator.service.ICalculatorService;
+import com.iteriam.SanitasCalculator.service.SubstractCalculatorService;
 
 @RestController
 public class OperatorController {
 
+	ICalculatorService calculator;
+	
 	@PostMapping(path= "/substract", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> substract(@RequestBody BasicOperandModel operand) {
-
-			return new ResponseEntity<>(null);
+		calculator = new SubstractCalculatorService(); 
+		operand = calculator.calculate(operand);
+				
+		return new ResponseEntity<>(operand.getResult(), HttpStatus.OK);
 	}
 	
 	@PostMapping(path= "/add", consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> add(@RequestBody BasicOperandModel operand) {
-
-			return new ResponseEntity<>(null);
+		calculator = new AddCalculatorService();
+		operand = calculator.calculate(operand);
+		
+		return new ResponseEntity<>(operand.getResult(), HttpStatus.OK);
 	}
 }
